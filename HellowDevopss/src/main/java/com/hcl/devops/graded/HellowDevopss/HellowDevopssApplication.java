15e5a23f2d11a4126d7e919968707f906b0c9e96
navyavishnumolakala <navyavishnumolakala99@gmail.com>
2022-01-21 13:43:35 +0530

    done

---
+++ HellowDevopss/src/main/java/com/hcl/devops/graded/HellowDevopss/HellowDevopssApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.devops.graded.HellowDevopss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowDevopssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowDevopssApplication.class, args);
+		System.out.println("Hello DevOps");
+	}
+
+}
---
+++ HellowDevopss/src/test/java/com/hcl/devops/graded/HellowDevopss/HellowDevopssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.devops.graded.HellowDevopss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowDevopssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
